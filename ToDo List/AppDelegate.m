f3e66ae1af803765cdccc1dd7e809dfcf8d69351
Admin <admin@Mac-Admin.local>
2016-07-14 17:08:07 +0500

    -подключил MagicalRecord -подключил CoreData

--- ToDo List/AppDelegate.m
+++ ToDo List/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <MagicalRecord/MagicalRecord.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [MagicalRecord setupCoreDataStackWithStoreNamed:@"ToDoList.sqlite"];
+    
     // Override point for customization after application launch.
     return YES;
 }
