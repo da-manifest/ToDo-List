642d62192e5b5c179427e68330ad48545694d2ff
Admin <admin@Mac-Admin.local>
2016-07-14 15:04:27 +0500

    - начало проекта

---
+++ ToDo List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDo List
+//
+//  Created by Admin on 14/07/16.
+//  Copyright © 2016 da_manifest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDo List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDo List
+//
+//  Created by Admin on 14/07/16.
+//  Copyright © 2016 da_manifest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
